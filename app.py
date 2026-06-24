adec0d5bcb56978b07ae37fa4ca8faca401a5435
SidramA1999 <67159886+SidramA1999@users.noreply.github.com>
2026-06-24 20:42:11 +0530

    app.py

--- app.py
+++ app.py
@@ -594,9 +594,12 @@ def prepare_batches():
 
 
 # ✅ ✅ ✅ PROPER APP START (FINAL)
+import os
+    
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
         create_initial_batches()   # ✅ runs only if empty
 
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
