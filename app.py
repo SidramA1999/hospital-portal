6e088410671edb97928a6258372b173dbc272a47
SidramA1999 <67159886+SidramA1999@users.noreply.github.com>
2026-06-27 11:59:40 +0530

    app.py

--- app.py
+++ app.py
@@ -290,25 +290,6 @@ def prepare_batches():
 def success():
     return render_template("success.html")
 
-
-#---@@@ UI chatBot------
-@app.route("/chat", methods=["POST"])
-def chat():
-    user_msg = request.json.get("message")
-
-    response = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-            {
-                "role": "system",
-                "content": "Answer ONLY from hospital rules. Otherwise say contact hospital."
-            },
-            {"role": "user", "content": user_msg}
-        ]
-    )
-
-    return {"reply": response.choices[0].message.content}
-
 ##------AI 
 from openai import OpenAI
 client = OpenAI(api_key="YOUR_API_KEY")
